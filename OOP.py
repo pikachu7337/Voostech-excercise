ed9217c500d55b82fcb00d0e551ebb3ceed00cdb
Ibrahim Aliyu <92918286+pikachu7337@users.noreply.github.com>
2021-10-28 14:20:28 +0100

    Add files via upload

---
+++ OOP.py
@@ -0,0 +1,33 @@
+
+
+
+class Vehicle:
+
+    def __init__(self, name, make, price):
+        self.name_ = name_of_the _car:
+        self.make_ = make_of_car
+        self.price_ = price_of_car
+
+
+        def name(self):
+            return "The name of the car is {benz}".format(self.name_)
+        def make(self):
+                return "The make of the car is {}".format(self.make_)
+
+        def price(self):
+                    return "The price of the car is {}".format(self.price_)
+        def car_life_left(self):
+            return "This car has {} years left on the road!".format(40 - self.age_)   
+
+
+
+            car = Vehicle('Benz, make, 2000000')
+            print (Car.car_life_left())
+            print(car.name())
+            print(car.price())
+
+
+
+
+
+           
---
+++ greene.py
@@ -0,0 +1,12 @@
+x  = 3
+f = 3.1415926
+name = "Python"
+print(x)
+print(f)
+print(name)
+combination = name + " " + name
+print(combination)
+sum = f + f
+print(sum)
+
+print("This is the first task. \This is the second task.")
